8807f300585da3c9c8f08aafe22a1842dbaca69a
Mark J. Titorenko <mark.titorenko@alces-software.com>
2012-01-02 21:58:17 +0000

    Version bump to 0.1.0

---
+++ lib/elfinder-rails/version.rb
@@ -0,0 +1,10 @@
+#==============================================================================
+# Copyright (C) 2012 Stephen F Norledge & Alces Software Ltd.
+#
+# This file is part of elfinder-rails.
+#
+# Some rights reserved, see LICENSE.txt.
+#==============================================================================
+module ElfinderRails
+  VERSION = "0.1.0"
+end
