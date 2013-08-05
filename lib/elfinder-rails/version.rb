1373298c4ce4e2eb478e7bc9bdc2b44a8626a199
Mark J. Titorenko <mark.titorenko@alces-software.com>
2013-08-05 11:14:39 +0100

    Version bump to 0.3.0

--- lib/elfinder-rails/version.rb
+++ lib/elfinder-rails/version.rb
@@ -6,5 +6,5 @@
 # Some rights reserved, see LICENSE.txt.
 #==============================================================================
 module ElfinderRails
-  VERSION = "0.3.0.dev"
+  VERSION = "0.3.0"
 end
