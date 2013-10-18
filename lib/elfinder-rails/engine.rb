9ee6777768dc38f9155075273881c4cbf7806915
James Donohue <james.donohue@alces-software.com>
2013-10-18 18:22:11 +0100

    Update copyright notice

--- lib/elfinder-rails/engine.rb
+++ lib/elfinder-rails/engine.rb
@@ -1,7 +1,7 @@
 #==============================================================================
 # Copyright (C) 2012-2013 Stephen F Norledge & Alces Software Ltd.
 #
-# This file is part of elfinder-rails.
+# This file is part of elfinder-rails-connector.
 #
 # Some rights reserved, see LICENSE.txt.
 #==============================================================================
